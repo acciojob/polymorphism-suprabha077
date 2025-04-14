48fcf5eef7377ca05702055bad7d69a26d27bc73
suprabha077 <84761170+suprabha077@users.noreply.github.com>
2025-04-14 13:35:07 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,14 @@ package com.driver;
 
 public class Main {
 
+
+  public static void main(String args[]){
+
+    performproduct performproduct = new performproduct();
+
+    performproduct.product(x:4,y:5);
+    performproduct.product(x:6,y:6,z:7);
+    performproduct.product(x:34.67,y:78.98);
+  }
+
 }

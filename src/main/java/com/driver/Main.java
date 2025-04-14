784a6678bf55fe4b12d2155dbf92535010c939f1
suprabha077 <84761170+suprabha077@users.noreply.github.com>
2025-04-14 14:16:33 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,12 +5,12 @@ public class Main {
 
   public static void main(String[] args) {
         // Task 2: Create object of Product
-        Product p = new Product();
+        TestCases p = new TestCases();
 
         // Call all product methods
-        System.out.println("Product of 4 and 5: " + p.product(4, 5));            // int x, int y
-        System.out.println("Product of 2, 3 and 4: " + p.product(2, 3, 4));     // int x, int y, int z
-        System.out.println("Product of 2.5 and 3.5: " + p.product(2.5, 3.5));   // double x, double y
+        System.out.println("Product of 4 and 5: " + p.TestCases(4, 5));            // int x, int y
+        System.out.println("Product of 2, 3 and 4: " + p.TestCases(2, 3, 4));     // int x, int y, int z
+        System.out.println("Product of 2.5 and 3.5: " + p.TestCases(2.5, 3.5));   // double x, double y
     }
 }
 

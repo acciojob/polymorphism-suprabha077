071b5caa318093067570cce13723207195635993
suprabha077 <84761170+suprabha077@users.noreply.github.com>
2025-04-14 14:05:23 +0530

    Update TestCases.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -5,17 +5,15 @@ import org.junit.jupiter.api.Test;
 
 public class TestCases {
 
-    public class TestCases {
-
-    public static void product(int x, int y) {
+    public  void product(int x, int y) {
         System.out.println(x * y);
     }
 
-    public static void product(int x, int y, int z) {
+    public  void product(int x, int y, int z) {
         System.out.println(x * y * z);
     }
 
-    public static void product(double x, double y) {
+    public  void product(double x, double y) {
         System.out.println(x * y);
     }
 }
